01e22ff293318c2ed83ef0d083c91eee11752a89
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-20 12:40:07 +0300

    Just making sure I understand LOL!

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def restafood(request):
+	context = {
+		'msg':"Hello World!",
+	}
+	return render(request, 'foodpage.html',context)
